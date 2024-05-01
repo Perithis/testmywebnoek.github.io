74f3edc38b1c82b3c110e0e20fd669390c9ac49b
Aleksandr Veselov <123272405+Perithis@users.noreply.github.com>
2024-05-01 12:19:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ let prizes =
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-let usercard = document.getElementById("username");
-usercard.innerText = `${tg.initDataUnsafe.user.username}`;
+// let usercard = document.getElementById("username");
+// usercard.innerText = `${tg.initDataUnsafe.user.username}`;
 
 function rotateFunction(){
     var min = 1024;
