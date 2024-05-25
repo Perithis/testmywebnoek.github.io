1604ccae9216f05c55d31cb3aaa223d32af2343d
Aleksandr Veselov <123272405+Perithis@users.noreply.github.com>
2024-05-25 13:34:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let prizes =
 ];
 
 let tg = window.Telegram.WebApp;
+tg.expand()
 
 // let usercard = document.getElementById("username");
 // usercard.innerText = `${tg.initDataUnsafe.user.username}`;
